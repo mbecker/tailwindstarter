0adca7e2455e4751268f1924dbfda7c171dd3975
Mats Becker@Macbook13 <mats.becker@gmail.com>
2022-03-09 22:19:21 +0100

    Updates for oauth

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+const comments = require("postcss-discard-comments");
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    }
-  };
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    cssnano({
+      preset: "default",
+    }),
+    purgecss({
+      content: ["./src/**/*.html", "./src/**/*.js"],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+    comments({removeAll: true})
+  ],
+};
